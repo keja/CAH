a62e441ed05c4b9c8a03568dd4f28d2f35c4e1d1
= <=>
2018-03-18 22:21:40 +0100

    added socket server

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,4 +1,14 @@
-import fs from 'fs';
+import http from 'http';
+import socket from 'socket.io';
+
+const server = http.createServer();
+const io = socket(server);
+
+io.on('connection', function(client){
+  console.log("wee");
+  client.on('event', function(data){});
+  client.on('disconnect', function(){});
+});
 
 
-console.log('hje');
+server.listen(3000);
